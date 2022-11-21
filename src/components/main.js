3a039668e33f450f8fed30982b4b14bffd8e11cf
Henry Naptal <henrynaptal@users.noreply.github.com>
2022-11-21 17:39:32 +0200

    Main algeline disain tehtud

--- src/components/main.js
+++ src/components/main.js
@@ -2,31 +2,37 @@ import React, { Component } from 'react'
 export default class Main extends Component {
     render() {
         return (
+            <div class = "stat-box">
             <form>
-                <h3>MAIN</h3>
                 <div class="statistics">
 
-                    <div class="workout-amount">
+                    <div class="stats">
                         <h3>Workouts completed:</h3>
                         <p>-data-</p>
                     </div>
 
-                    <div class="total-weight">
+                    <div class="stats">
                         <h3>Total weight lifted this week:</h3>
                         <p>-data-</p>
                     </div>
 
-                    <div class="improvement">
+                    <div class="stats">
                         <h3>Biggest improvement this month:</h3>
                         <p>-data-</p>
                     </div>
 
-                    <div class="distance-ran">
+                    <div class="stats">
                         <h3>Distance ran this week:</h3>
                         <p>-data-</p>
                     </div>
                 </div>
             </form>
+            <form class = "notes">
+                <div class="records">
+                    <h3>Your gym notes:</h3>
+                </div>
+            </form>
+            </div>
         )
     }
 }
